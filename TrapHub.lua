bf151bbb5ea56047d7fe35f3087e3698e255ad6e
Lhymdz <105396828+Lhymdz@users.noreply.github.com>
2022-10-08 07:57:28 -0300

    Delete TrapHub.lua

--- TrapHub.lua
+++
@@ -1,7 +0,0 @@
-if game.PlaceId == 8304191830 or game.PlaceId == 8349889591 then
-	pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Lhymdz/lhymdz/main/animeadv.lua"))() 
-	end)
-end
-
-
