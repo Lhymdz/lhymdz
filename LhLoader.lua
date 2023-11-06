1506776a9ed4dba488bdfff5e2de52cadf24c580
Lhymdz <105396828+Lhymdz@users.noreply.github.com>
2023-11-06 17:47:24 -0300

    Add files via upload

---
+++ LhLoader.lua
@@ -0,0 +1,17 @@
+if game.PlaceId == 14565278851 then
+    print("Anime Skill")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lhymdz/lhymdz/main/AnimeSkill.lua"))()
+elseif game.PlaceId == 15084364495 then
+    print("Bike Race Simulatort")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lhymdz/lhymdz/main/bicicleta.lua"))()
+elseif game.PlaceId == 9144187696 then
+    print("OP Magic Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lhymdz/lhymdz/main/MagicBoxer.lua"))()
+elseif game.PlaceId == 14762790937 then
+    print("Anime Waves Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lhymdz/lhymdz/main/ondinha.lua"))()
+elseif game.PlaceId == 9144187696 then
+    print("Fighting Legends")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lhymdz/lhymdz/main/Espadinha.lua"))()
+else
+end
